6b629f259d562dd63c8a875a20723350b3d59fe4
Sun4n <kazisunan68@gmail.com>
2026-04-24 17:56:09 +0600

    build navbar and giving some style

---
+++ src/Component/NavBar/NavBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FaCartPlus } from 'react-icons/fa';
+
+const NavBar = () => {
+    return (
+        <>
+            <div className="flex justify-between items-center bg-base-100 py-4 mx-[200px]">
+                <div>
+                    <img src="/src/assets/DigiTools.png" alt="" />
+                </div>
+                <div>
+                    <ul className='flex gap-4 font-semibold'>
+                        <li>Products</li>
+                        <li>Features</li>
+                        <li>Pricing</li>
+                        <li>Testimonials</li>
+                        <li>FAQ</li>
+                    </ul>
+                </div>
+                <div className='flex items-center gap-4'>
+                    <FaCartPlus/>
+                    <p className='font-semibold'>Login</p>
+                    <button className='text-[1rem] font-semibold h-14 bg-linear-65 from-[#4F39F6] to-[#9514FA] rounded-[10000px] px-[16px] py-[12px] text-white'>Get Started</button>
+                </div>
+            </div>
+            <div className="divider"></div>
+        </>
+    );
+};
+
+export default NavBar;
